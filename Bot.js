b81204447b21f41c2fb8a70dee1f0fd85974ee82
Tashi Tsering KC <43880377+PosthumasFrankestein@users.noreply.github.com>
2022-02-21 09:29:31 +0545

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -16,7 +16,7 @@ class Bot {
     subBots = {};
 
     constructor(token) {
-        this.token = "NzIwNDY4MjA0MjQ1NDgzNTYz.XuGaTQ.2HvXFX7O7-SQOV3tKtrOO6rWCWI";
+        this.token = "NzIwNDY4MjA0MjQ1NDgzNTYz.XuGaTQ.Hng5sfHAG-lbOz77Pi-0jEOVP6k";
         this.bot = new Client();
         this.nyaa = new NyaaProvider();
         this.jpop = new JpopSukiProvider();
